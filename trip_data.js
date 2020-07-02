4ed3387ebd99954ce2051411713c6d7ba38b89bb
Virendra Singh <veeermca2010@gmail.com>
2020-07-02 22:27:51 +0530

    d3.js app

---
+++ trip_data.js
@@ -0,0 +1,20 @@
+var data = [
+{"lat" : 55.859234, 
+ "lon" : -4.244489, 
+ "name" : "Glasgow, Scotland",
+ "country" : "036",
+ "posts" : {
+	"solar":"solar",
+	"wind": "wind",
+	"NoStory": "NoStory"
+	 }},
+	 {"lat" : 55.950448, 
+ "lon" : -3.180290, 
+ "name" : "Edinburgh, Scotland",
+ "country" : "826",
+ "posts" : {
+	"wind": "wind",
+	"solar":"solar",
+	"NoStory": "NoStory"
+	 }},
+	 ];
